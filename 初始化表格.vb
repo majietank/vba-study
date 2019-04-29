579f5974d1ac3b94064aa63fec622280a8a73bd4
majietank <majie.tank@outlook.com>
2019-04-29 14:54:19 +0800

    2.1

--- 初始化表格.vb
+++ 初始化表格.vb
@@ -1,9 +1,13 @@
+'**********************************************************************************************
+'       制作总表模块
+'**********************************************************************************************
+'按钮
 Sub 表格数据初始化()
 Dim i As Integer
 Dim chepai As String
 Dim riqi As Date
 Sheet99.Activate
-'判断日期
+    '判断日期
 If Day(Date) > 25 Then
     riqi = Date
 Else
@@ -13,7 +17,7 @@ Sheet99.Cells(1, 1) = Year(riqi) & "年"
 Sheet99.Cells(1, 2) = Month(riqi) & "月"
 Call 车牌行
 Call 日期列(riqi, 3)
-'循环表
+    '循环表
 For i = 1 To Sheets.Count - 1
     With Sheets(i).Activate
         Call 记录信息(riqi)
@@ -23,6 +27,8 @@ For i = 1 To Sheets.Count - 1
     Sheet99.Activate
 Next i
 End Sub
+'----------------------------------------------------------------------------------------------
+'调用
 Sub 日期列(riqi As Date, qishi As Integer)
 Dim i As Integer
 Dim riqirow As Date
@@ -40,6 +46,8 @@ For i = 1 To 31
     End If
 Next i
 End Sub
+'----------------------------------------------------------------------------------------------
+'调用
 Sub 车牌行()
 Dim i As Integer
 Dim chepai As String
@@ -48,6 +56,8 @@ For i = 1 To Sheets.Count - 1
     Cells(2, i + 1) = chepai
 Next i
 End Sub
+'----------------------------------------------------------------------------------------------
+'调用
 Sub 记录信息(riqi As Date)
 Cells(3, "O") = Year(riqi) & "年"
 Cells(3, "Q") = Month(riqi) & "月"
@@ -56,6 +66,9 @@ Cells(47, "M") = Null
 Cells(47, "P") = Null
 End Sub
 '**********************************************************************************************
+'       单元格格式模块
+'**********************************************************************************************
+'按钮
 Sub 表格格式初始化()
 Dim i As Integer
 For i = 1 To Sheets.Count - 1
@@ -71,8 +84,7 @@ With Sheets(i).Activate
     ActiveWindow.ScrollRow = 1
     Range("A1:S51").Font.Name = "宋体"
     Range("A1:S51").NumberFormatLocal = "G/通用格式"
-    Range("A1:S51").Cells.Replace What:=" ", Replacement:="", LookAt:=xlPart, SearchOrder:= _
-        xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Range("A1:S51").Cells.Replace What:=" ", Replacement:="", LookAt:=xlPart, SearchOrder:= xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
     Rows("48:51").RowHeight = 15
     Cells(48, "A").Orientation = xlVertical
     Cells(5, "A").Orientation = xlVertical
@@ -80,6 +92,9 @@ End With
 Next i
 End Sub
 '**********************************************************************************************
+'       打印、存储模块
+'**********************************************************************************************
+'按钮
 Sub 打印存档()
 Dim i As Integer
 Dim owjm As String, wjm As String
@@ -99,6 +114,8 @@ Sheets("Sheet1").Delete
 ActiveWorkbook.Save
 ActiveWorkbook.Close
 End Sub
+'----------------------------------------------------------------------------------------------
+'按钮
 Sub 打印表格()
 Dim i As Integer
 For i = 1 To Sheets.Count - 1
@@ -109,6 +126,8 @@ End If
 Sheet99.Activate
 Next i
 End Sub
+'----------------------------------------------------------------------------------------------
+'按钮
 Sub 记录存档()
 Dim i As Integer
 Dim chepai As String
@@ -131,6 +150,9 @@ ActiveWorkbook.Save
 ActiveWorkbook.Close
 End Sub
 '**********************************************************************************************
+'                                   记录录入模块
+'**********************************************************************************************
+'按钮
 Sub 记录录入()
 Dim i As Integer, chewei As Integer
 Dim chepai As String
@@ -147,6 +169,8 @@ For i = 1 To Sheets.Count - 1
     Sheet99.Activate
 Next i
 End Sub
+'----------------------------------------------------------------------------------------------
+'调用
 Sub 记录判断()
 Dim i  As Integer
 For i = 16 To 46
@@ -161,6 +185,9 @@ End If
 Next i
 End Sub
 '**********************************************************************************************
+'                                          新增设备模块
+'**********************************************************************************************
+'按钮
 Sub 新增设备()
 Dim chepai As String
 chepai = InputBox("输入车牌：")
