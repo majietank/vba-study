00b568399f49afcec4ec288fea55b4e81246b407
majietank <majie.tank@outlook.com>
2019-04-29 09:19:30 +0800

    1.1

--- 初始化表格.vb
+++ 初始化表格.vb
@@ -69,3 +69,32 @@ Sub 打印表格()
 ActiveWindow.SelectedSheets.PrintOut Copies:=1, Collate:=True, _
         IgnorePrintAreas:=False
 End Sub
+Sub 记录录入()
+Dim i As Integer, chewei As Integer
+Dim chepai As String
+Sheet99.Activate
+For i = 1 To Sheets.Count - 1
+    chepai = Sheets(i).Name
+    chewei = Sheet99.Rows(2).Find(chepai).Column
+    Range(Cells(3, chewei), Cells(33, chewei)).Copy
+    With Sheets(i).Activate
+        Range("P16").PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+        :=False, Transpose:=False
+        Call 记录判断
+    End With
+    Sheet99.Activate
+Next i
+End Sub
+Sub 记录判断()
+Dim i  As Integer
+For i = 16 To 46
+If Cells(i, 1) <> 0 Then
+    If Cells(i, "P") <> 0 Then
+        Range(Cells(i, "B"), Cells(i, "O")) = "√"
+        Cells(i, "S") = Null
+    Else
+        Cells(i, "S") = "○"
+    End If
+End If
+Next i
+End Sub
