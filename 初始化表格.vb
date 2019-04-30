dec550d3336591362175e8b2aa4456a1e1f48522
majietank <majie.tank@outlook.com>
2019-04-30 10:35:20 +0800

    2.4

--- 初始化表格.vb
+++ 初始化表格.vb
@@ -15,7 +15,7 @@ Else
 End If
 Sheet99.Cells(1, 1) = Year(riqi) & "年"
 Sheet99.Cells(1, 2) = Month(riqi) & "月"
-Call 车牌行
+Call 车牌汇总行
 Call 日期列(riqi, 3)
     '循环表
 For i = 1 To Sheets.Count - 1
@@ -48,12 +48,15 @@ Next i
 End Sub
 '----------------------------------------------------------------------------------------------
 '调用
-Sub 车牌行()
+Sub 车牌汇总行()
 Dim i As Integer
 Dim chepai As String
+Dim chejilu As Range
 For i = 1 To Sheets.Count - 1
     chepai = Sheets(i).Name
     Cells(2, i + 1) = chepai
+    Set chejilu = Range(Cells(3, i + 1), Cells(33, i + 1))
+    Cells(34, i + 1) = WorksheetFunction.CountA(chejilu)
 Next i
 Rows(2).HorizontalAlignment = xlCenter
 Rows(2).VerticalAlignment = xlCenter
@@ -78,28 +81,64 @@ Dim i As Integer
 For i = 1 To Sheets.Count - 1
 With Sheets(i).Activate
     Cells(1, 1).Select
-    'ActiveWindow.View = xlPageBreakPreview
-    ActiveWindow.View = xlPageLayoutView
+    ActiveWindow.View = xlPageBreakPreview
+    'ActiveWindow.View = xlPageLayoutView
+    ActiveWindow.Zoom = 100
     Columns("B:S").ColumnWidth = 3.35
     Columns("A").ColumnWidth = 8.38
     Rows("1:1").RowHeight = 25
     Rows("2:51").RowHeight = 15
-    ActiveWindow.Zoom = 100
-    ActiveWindow.ScrollColumn = 1
-    ActiveWindow.ScrollRow = 1
     Range("A1:S51").Font.Name = "宋体"
     Range("A1:S51").NumberFormatLocal = "G/通用格式"
     Range("A1:S51").Cells.Replace What:=" ", Replacement:="", LookAt:=xlPart, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
     Rows("48:51").RowHeight = 15
     Cells(48, "A").Orientation = xlVertical
     Cells(5, "A").Orientation = xlVertical
-    LeftMargin = Application.InchesToPoints(0.708661417322835)
-    RightMargin = Application.InchesToPoints(0.708661417322835)
-    TopMargin = Application.InchesToPoints(0.354330708661417)
-    BottomMargin = Application.InchesToPoints(0.275590551181102)
-    HeaderMargin = Application.InchesToPoints(0.31496062992126)
-    FooterMargin = Application.InchesToPoints(0.31496062992126)
+    '页面修改
+        LeftHeader = ""
+        CenterHeader = ""
+        RightHeader = ""
+        LeftFooter = ""
+        CenterFooter = ""
+        RightFooter = ""
+        LeftMargin = Application.InchesToPoints(0.708661417322835)
+        RightMargin = Application.InchesToPoints(0.708661417322835)
+        TopMargin = Application.InchesToPoints(0.354330708661417)
+        BottomMargin = Application.InchesToPoints(0.275590551181102)
+        HeaderMargin = Application.InchesToPoints(0.31496062992126)
+        FooterMargin = Application.InchesToPoints(0.31496062992126)
+        PrintHeadings = False
+        PrintGridlines = False
+        PrintComments = xlPrintNoComments
+        PrintQuality = 600
+        CenterHorizontally = False
+        CenterVertically = False
+        Orientation = xlPortrait
+        Draft = False
+        PaperSize = xlPaperA4
+        FirstPageNumber = xlAutomatic
+        Order = xlDownThenOver
+        BlackAndWhite = False
+        Zoom = 100
+        PrintErrors = xlPrintErrorsDisplayed
+        OddAndEvenPagesHeaderFooter = False
+        DifferentFirstPageHeaderFooter = False
+        ScaleWithDocHeaderFooter = True
+        AlignMarginsHeaderFooter = True
+'        EvenPage.LeftHeader.Text = ""
+'        EvenPage.CenterHeader.Text = ""
+'        EvenPage.RightHeader.Text = ""
+'        EvenPage.LeftFooter.Text = ""
+'        EvenPage.CenterFooter.Text = ""
+'        EvenPage.RightFooter.Text = ""
+'        FirstPage.LeftHeader.Text = ""
+'        FirstPage.CenterHeader.Text = ""
+'        FirstPage.RightHeader.Text = ""
+'        FirstPage.LeftFooter.Text = ""
+'        FirstPage.CenterFooter.Text = ""
+'        FirstPage.RightFooter.Text = ""
 End With
+MsgBox 1
 Next i
 End Sub
 '**********************************************************************************************
